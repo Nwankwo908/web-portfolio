5a0f359dd1580965159c0a3db4ebc40ccf2774f8
Nwankwo908 <nwankwo908@gmail.com>
2026-03-09 21:16:00 -0400

    Revert "Merge pull request #2 from Nwankwo908/codex/verify-github-pages-deployment-setup"
    This reverts commit 04e97615aee7b95fadc016d4c57dc5da8af36a69, reversing
    changes made to 21e484bd1509a0b106b5a97297a92b744c81f347.

--- web-portfolio/src/main.jsx
+++ web-portfolio/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WebPortfolio from "./webPortfolio.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WebPortfolio />
+    <App />
   </React.StrictMode>
 );
